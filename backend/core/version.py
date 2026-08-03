2f0544e1647b6c765d2b0ce332b0da4a65d3cb4c
dkkhare <devendra.kkhare@gmail.com>
2026-08-03 22:06:55 +0530

    Create backend/core/version.py

---
+++ backend/core/version.py
@@ -0,0 +1,15 @@
+"""
+Application version information.
+"""
+
+APP_NAME = "AI Content Studio"
+
+APP_VERSION = "0.1.0"
+
+AUTHOR = "Devendra Khare"
+
+COPYRIGHT = "Copyright (c) 2026"
+
+GITHUB = "https://github.com/dkkhare/AI-Content-Studio"
+
+BUILD = "001"
