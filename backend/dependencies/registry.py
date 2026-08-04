a674d1097d00a2592e5e922ac271bc3b14f8d3c8
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 12:48:25 +0530

    Create registry.py

---
+++ backend/dependencies/registry.py
@@ -0,0 +1,36 @@
+from backend.dependencies.models import AIModel
+
+
+MODELS = [
+
+    AIModel(
+        "F5-TTS",
+        "1.0"
+    ),
+
+    AIModel(
+        "LivePortrait",
+        "1.0"
+    ),
+
+    AIModel(
+        "WhisperX",
+        "3.1"
+    ),
+
+    AIModel(
+        "PaddleOCR",
+        "2.8"
+    ),
+
+    AIModel(
+        "SAM2",
+        "1.0"
+    ),
+
+    AIModel(
+        "RealESRGAN",
+        "1.0"
+    )
+
+]
