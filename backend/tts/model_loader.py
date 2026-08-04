6ae32a3e8b8b2dc9b649840ea12e120e07e23299
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 10:46:06 +0530

    Create model_loader.py

---
+++ backend/tts/model_loader.py
@@ -0,0 +1,17 @@
+class ModelLoader:
+
+    def __init__(self):
+
+        self.loaded = False
+
+    def load(self):
+
+        """
+        Load F5-TTS model.
+
+        Actual loading logic will be
+        implemented after dependency
+        integration.
+        """
+
+        self.loaded = True
