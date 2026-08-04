46dc1ecb73138820277de7c8bde073c67520c40c
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 10:33:55 +0530

    Create provider_registry.py

---
+++ backend/tts/provider_registry.py
@@ -0,0 +1,13 @@
+class ProviderRegistry:
+
+    def __init__(self):
+
+        self.providers = {}
+
+    def register(self, provider):
+
+        self.providers[provider.name] = provider
+
+    def get(self, name):
+
+        return self.providers[name]
