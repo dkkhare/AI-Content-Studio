d036fa3fb6cb8b817ff8f25a5a9aab09e36bde61
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 19:19:31 +0530

    Update __init__.py

--- backend/tts/__init__.py
+++ backend/tts/__init__.py
@@ -1,7 +1,13 @@
 from .manager import TTSManager
 from .generator import TTSGenerator
+from .audio_merger import AudioMerger
 
 __all__ = [
+
     "TTSManager",
+
     "TTSGenerator",
+
+    "AudioMerger",
+
 ]
