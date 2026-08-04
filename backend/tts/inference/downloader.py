843109029fe5830b609f97767a69f5ee542e8220
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 10:57:58 +0530

    Create downloader.py

---
+++ backend/tts/inference/downloader.py
@@ -0,0 +1,15 @@
+class ModelDownloader:
+
+    """
+    Responsible for downloading
+    model files.
+
+    Initial implementation is a stub.
+    """
+
+    def download(self):
+
+        raise NotImplementedError(
+            "Download logic will be implemented "
+            "during model integration."
+        )
