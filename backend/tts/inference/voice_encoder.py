6ff9583b1b15a045a4a0d16716b18fb3cb60082a
dkkhare <devendra.kkhare@gmail.com>
2026-08-04 11:01:55 +0530

    Create voice_encoder.py

---
+++ backend/tts/inference/voice_encoder.py
@@ -0,0 +1,12 @@
+class VoiceEncoder:
+
+    """
+    Future voice embedding extraction.
+
+    Placeholder until F5-TTS
+    integration.
+    """
+
+    def encode(self, reference_audio):
+
+        return reference_audio
