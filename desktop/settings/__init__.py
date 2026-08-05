70643e501e73e0f1160fdf31bd257a2222f52d0a
dkkhare <devendra.kkhare@gmail.com>
2026-08-05 10:57:24 +0530

    Update __init__.py

--- desktop/settings/__init__.py
+++ desktop/settings/__init__.py
@@ -1,5 +1,9 @@
 from .settings_manager import SettingsManager
+from .recent_projects import RecentProjects
+from .ui_state import UIState
 
 __all__ = [
     "SettingsManager",
+    "RecentProjects",
+    "UIState",
 ]
