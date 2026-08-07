3a82b8fd3ecce4f2ea52fe47282fbc1835d1b023
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 04:56:16 +0530

    Export local scene video services

--- backend/video/__init__.py
+++ backend/video/__init__.py
@@ -1,10 +1,17 @@
+from .episode_assembler import EpisodeVideoAssembler
 from .ffmpeg_renderer import FFmpegRenderer
-from .local_provider import LocalCommandVideoProvider, VideoGenerationRequest
+from .local_provider import LocalCommandVideoProvider, VideoGenerationRequest, create_local_video_provider
 from .manager import VideoGenerationManager
+from .review import VideoAssetReviewStore
+from .scene_service import SceneVideoService
 
 __all__ = [
+    "EpisodeVideoAssembler",
     "FFmpegRenderer",
     "LocalCommandVideoProvider",
     "VideoGenerationRequest",
+    "create_local_video_provider",
     "VideoGenerationManager",
+    "VideoAssetReviewStore",
+    "SceneVideoService",
 ]
