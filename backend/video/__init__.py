08d3236735dd6a1a5655e963311935d42a1e94c2
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 01:14:21 +0530

    Export local video generation APIs

--- backend/video/__init__.py
+++ backend/video/__init__.py
@@ -1,3 +1,10 @@
 from .ffmpeg_renderer import FFmpegRenderer
+from .local_provider import LocalCommandVideoProvider, VideoGenerationRequest
+from .manager import VideoGenerationManager
 
-__all__ = ["FFmpegRenderer"]
+__all__ = [
+    "FFmpegRenderer",
+    "LocalCommandVideoProvider",
+    "VideoGenerationRequest",
+    "VideoGenerationManager",
+]
