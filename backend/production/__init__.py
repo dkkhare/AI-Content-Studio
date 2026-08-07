1a1d84a5d16113e55edf8c9038d507ec83c212ef
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 05:07:36 +0530

    Add production package

---
+++ backend/production/__init__.py
@@ -0,0 +1,3 @@
+from .service import EpisodeProductionService, EpisodeSubtitleService, EpisodeThumbnailService
+
+__all__ = ["EpisodeProductionService", "EpisodeSubtitleService", "EpisodeThumbnailService"]
