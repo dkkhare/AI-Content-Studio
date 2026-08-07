54e43fcc9f0e9cdeb16d4f011ae7f267b99ed41e
dkkhare <devendra.kkhare@gmail.com>
2026-08-07 21:42:49 +0530

    Export desktop pipeline controller

--- desktop/controllers/__init__.py
+++ desktop/controllers/__init__.py
@@ -1,5 +1,9 @@
+from .pipeline_controller import PipelineController
+from .project_controller import ProjectController
 from .tts_controller import TTSController
 
 __all__ = [
+    "PipelineController",
+    "ProjectController",
     "TTSController",
 ]
