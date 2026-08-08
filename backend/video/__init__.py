4e05180a45a03ec10f1dfa43382e0e87eb0fbfef
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 10:21:15 +0530

    Export Milestone 17 video composition API

---
+++ backend/video/__init__.py
@@ -0,0 +1,10 @@
+from .core import CompositionManifest, MediaAsset, VideoComposer, VideoSpec
+from .project_service import ProjectVideoService
+
+__all__ = [
+    "CompositionManifest",
+    "MediaAsset",
+    "ProjectVideoService",
+    "VideoComposer",
+    "VideoSpec",
+]
