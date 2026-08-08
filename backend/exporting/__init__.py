095a2cce668b3db8752fee4e9d6ba14e608ed2b8
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 10:56:29 +0530

    Export batch queue API

--- backend/exporting/__init__.py
+++ backend/exporting/__init__.py
@@ -1,8 +1,12 @@
+from .batch import BatchExportJob, BatchExportQueue, BatchExportRunner
 from .core import PRESETS, ExportAsset, ExportManifest, ExportPreset
 from .service import ExportCancelled, ProjectExportService
 
 __all__ = [
     "PRESETS",
+    "BatchExportJob",
+    "BatchExportQueue",
+    "BatchExportRunner",
     "ExportAsset",
     "ExportCancelled",
     "ExportManifest",
