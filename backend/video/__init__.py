d4c4310b9d9f216e8cbb03a8e35336c70ab363be
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 10:24:01 +0530

    Export FFmpeg video rendering API

--- backend/video/__init__.py
+++ backend/video/__init__.py
@@ -1,10 +1,20 @@
 from .core import CompositionManifest, MediaAsset, VideoComposer, VideoSpec
+from .ffmpeg import (
+    FFmpegCommandBuilder,
+    FFmpegRenderer,
+    FFmpegRenderError,
+    RenderCancelled,
+)
 from .project_service import ProjectVideoService
 
 __all__ = [
     "CompositionManifest",
+    "FFmpegCommandBuilder",
+    "FFmpegRenderer",
+    "FFmpegRenderError",
     "MediaAsset",
     "ProjectVideoService",
+    "RenderCancelled",
     "VideoComposer",
     "VideoSpec",
 ]
