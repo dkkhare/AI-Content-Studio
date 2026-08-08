4ba3161d402439c2b18d3d257b749862a439c466
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 10:51:45 +0530

    Export cancellation exception

--- backend/exporting/__init__.py
+++ backend/exporting/__init__.py
@@ -1,9 +1,10 @@
 from .core import PRESETS, ExportAsset, ExportManifest, ExportPreset
-from .service import ProjectExportService
+from .service import ExportCancelled, ProjectExportService
 
 __all__ = [
     "PRESETS",
     "ExportAsset",
+    "ExportCancelled",
     "ExportManifest",
     "ExportPreset",
     "ProjectExportService",
