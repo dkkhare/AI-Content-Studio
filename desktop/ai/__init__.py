fc8e9d6af92051da82c490cde6cacc4889bf59e2
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 08:28:29 +0530

    Add Milestone 14 desktop AI configuration core

---
+++ desktop/ai/__init__.py
@@ -0,0 +1,4 @@
+from .configuration import AIProfile, AISettingsStore
+from desktop.controllers.ai_controller import AIDesktopController
+
+__all__ = ["AIProfile", "AISettingsStore", "AIDesktopController"]
