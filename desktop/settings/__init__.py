fb9c9aac2f5b035ad2e49ca6afc85552f6de25b8
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 12:09:40 +0530

    Expose update preferences

--- desktop/settings/__init__.py
+++ desktop/settings/__init__.py
@@ -1,9 +1,11 @@
 from .settings_manager import SettingsManager
 from .recent_projects import RecentProjects
 from .ui_state import UIState
+from .update_preferences import UpdatePreferences
 
 __all__ = [
     "SettingsManager",
     "RecentProjects",
     "UIState",
+    "UpdatePreferences",
 ]
