a0d3b209256f08426dbcee677cdfc5cac728ca75
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 05:41:03 +0530

    Export publishing provider APIs

--- backend/publishing/__init__.py
+++ backend/publishing/__init__.py
@@ -1,4 +1,19 @@
+from .publisher import PublishingService
+from .providers import (
+    ManualPublishingProvider,
+    PublishResult,
+    PublishingProvider,
+    YouTubePublishingProvider,
+)
 from .release import ReleaseManager
 from .service import PublishingMetadataService
 
-__all__ = ["PublishingMetadataService", "ReleaseManager"]
+__all__ = [
+    "PublishingMetadataService",
+    "ReleaseManager",
+    "PublishingService",
+    "PublishingProvider",
+    "PublishResult",
+    "ManualPublishingProvider",
+    "YouTubePublishingProvider",
+]
