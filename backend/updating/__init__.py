5719fc1885f6641b4c2d8ee0c05b73f5a0c504ef
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 12:06:31 +0530

    Expose verified installer download API

--- backend/updating/__init__.py
+++ backend/updating/__init__.py
@@ -1,9 +1,13 @@
+from .download import InstallerDownloader, UpdateCancelled, parse_checksum_manifest
 from .core import SemanticVersion, UpdateRelease
 from .service import GitHubReleaseClient, UpdateService
 
 __all__ = [
     "GitHubReleaseClient",
+    "InstallerDownloader",
     "SemanticVersion",
+    "UpdateCancelled",
     "UpdateRelease",
     "UpdateService",
+    "parse_checksum_manifest",
 ]
