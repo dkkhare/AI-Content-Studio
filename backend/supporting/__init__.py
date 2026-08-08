171c06204b5893db76d46ffd1298cd9dca1323fc
dkkhare <devendra.kkhare@gmail.com>
2026-08-08 13:10:45 +0530

    Expose support diagnostics API

---
+++ backend/supporting/__init__.py
@@ -0,0 +1,10 @@
+from .bundle import SupportAsset, SupportBundleService
+from .redaction import REDACTED, redact, redact_text
+
+__all__ = [
+    "REDACTED",
+    "SupportAsset",
+    "SupportBundleService",
+    "redact",
+    "redact_text",
+]
